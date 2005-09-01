7ac49589cb705f129253087a2f3e97e570d04214
Jaime Soriano Pastor <kronoss@users.sourceforge.net>
2005-09-01 10:02:47 +0000

    Added option to remount

--- pysdm/fsdata/ext3.py
+++ pysdm/fsdata/ext3.py
@@ -2,7 +2,7 @@ options = []
 options.append( \
 	{"acl":			[0, ["acl", ""], _("Support POSIX access list"), 1], \
 	 "bsddf":		[0, ["minixdf", ""], _("Minix behaviour for stat information"), 1], \
-	 "check":		[0, ["check", ""], _("Check filesystem at mount time"), 1], \
+	 "check":		[0, ["check", ""], _("Check file system at mount time"), 1], \
 	 "debug":		[0, ["debug", ""], _("Print debugging info"), 1], \
 	 "errors":		[2, ["continue", "remount-ro", "panic"], _("Specify what to do in case of error"), 0], \
 	 "grpid":		[0, ["grpid", ""], _("Get group id from the directory for a new file"), 1], \
@@ -13,9 +13,9 @@ options.append( \
 	 "resuid":		[1, [""], _("User allowed to use reserved space"), 1], \
 	 "sb":			[1, [""], _("Use a especific block as superblock"), 1], \
 	 "user_xattr":	[0, ["user_xattr", ""], _("Support extended attributes"), 0], \
-	 "journal":		[1, ["update"], _("Update journal or especify the inode where it is"), 1], \
+	 "journal":		[1, ["update"], _("Update journal or specify the inode where it is"), 1], \
 	 "noload":		[0, ["noload", ""], _("Do not load journal on mounting"), 0], \
-	 "data":		[2, ["ordered", "journal", "writeback"], _("Especify a journaling mode"), 1], \
+	 "data":		[2, ["ordered", "journal", "writeback"], _("Specify a journaling mode"), 1], \
 	 "commit":		[1, ["0"], _("Synchronize data and metadata every specified seconds, 0 for default (5s.)"), 1] \
 	} \
 	)
